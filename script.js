b72e2137b5d162f2cacd3ab566c0dabc0eb8d425
Yatharth singh <83268352+imyath6666@users.noreply.github.com>
2022-01-13 15:34:30 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-import Ball from "./Ball.js"
-import Paddle from "./Paddle.js"
+
 
 const ball = new Ball(document.getElementById("ball"))
 const playerPaddle = new Paddle(document.getElementById("player-paddle"))
@@ -47,3 +46,108 @@ document.addEventListener("mousemove", e => {
 })
 
 window.requestAnimationFrame(update)
+
+
+const INITIAL_VELOCITY = 0.025
+const VELOCITY_INCREASE = 0.00001
+
+export default class Ball {
+  constructor(ballElem) {
+    this.ballElem = ballElem
+    this.reset()
+  }
+
+  get x() {
+    return parseFloat(getComputedStyle(this.ballElem).getPropertyValue("--x"))
+  }
+
+  set x(value) {
+    this.ballElem.style.setProperty("--x", value)
+  }
+
+  get y() {
+    return parseFloat(getComputedStyle(this.ballElem).getPropertyValue("--y"))
+  }
+
+  set y(value) {
+    this.ballElem.style.setProperty("--y", value)
+  }
+
+  rect() {
+    return this.ballElem.getBoundingClientRect()
+  }
+
+  reset() {
+    this.x = 50
+    this.y = 50
+    this.direction = { x: 0 }
+    while (
+      Math.abs(this.direction.x) <= 0.2 ||
+      Math.abs(this.direction.x) >= 0.9
+    ) {
+      const heading = randomNumberBetween(0, 2 * Math.PI)
+      this.direction = { x: Math.cos(heading), y: Math.sin(heading) }
+    }
+    this.velocity = INITIAL_VELOCITY
+  }
+
+  update(delta, paddleRects) {
+    this.x += this.direction.x * this.velocity * delta
+    this.y += this.direction.y * this.velocity * delta
+    this.velocity += VELOCITY_INCREASE * delta
+    const rect = this.rect()
+
+    if (rect.bottom >= window.innerHeight || rect.top <= 0) {
+      this.direction.y *= -1
+    }
+
+    if (paddleRects.some(r => isCollision(r, rect))) {
+      this.direction.x *= -1
+    }
+  }
+}
+
+function randomNumberBetween(min, max) {
+  return Math.random() * (max - min) + min
+}
+
+function isCollision(rect1, rect2) {
+  return (
+    rect1.left <= rect2.right &&
+    rect1.right >= rect2.left &&
+    rect1.top <= rect2.bottom &&
+    rect1.bottom >= rect2.top
+  )
+}
+
+
+const SPEED = 0.02
+
+export default class Paddle {
+  constructor(paddleElem) {
+    this.paddleElem = paddleElem
+    this.reset()
+  }
+
+  get position() {
+    return parseFloat(
+      getComputedStyle(this.paddleElem).getPropertyValue("--position")
+    )
+  }
+
+  set position(value) {
+    this.paddleElem.style.setProperty("--position", value)
+  }
+
+  rect() {
+    return this.paddleElem.getBoundingClientRect()
+  }
+
+  reset() {
+    this.position = 50
+  }
+
+  update(delta, ballHeight) {
+    this.position += SPEED * delta * (ballHeight - this.position)
+  }
+}
